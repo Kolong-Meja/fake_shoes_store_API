33eb3b5ec27cebc72f9962b890ce0935f8b01ba3
Faisal Ramadhan <faisalramadhan041201@gmail.com>
2022-12-27 22:47:23 +0700

    updates on v-0.1

--- app/Http/Controllers/ProductControllerAPI.php
+++ app/Http/Controllers/ProductControllerAPI.php
@@ -4,7 +4,10 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Product;
-use App\Resources\ProductResource;
+use Symfony\Component\HttpFoundation\Response as HttpFoundationResponse;
+use Facade\FlareClient\Http\Response;
+use Illuminate\Database\QueryException;
+use Illuminate\Support\Facades\Validator;
 
 class ProductControllerAPI extends Controller
 {
@@ -15,10 +18,10 @@ class ProductControllerAPI extends Controller
      */
     public function index()
     {
-        $products = Product::latest()->paginate(10)->get();
+        $products = Product::select('*')->latest('created_at')->paginate(5);
         $response = [
-            'message' => 'Product Data',
-            'data' => $product,
+            'message' => 'List of Shoes Data',
+            'data' => $products,
         ];
         return response()->json($response, HttpFoundationResponse::HTTP_OK);
     }
@@ -31,7 +34,34 @@ class ProductControllerAPI extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $except_data = ['id', 'created_at', 'updated_at'];
+
+        $validator = Validator::make($request->except($except_data), [
+            'nama' => 'required|string', 
+            'harga' => 'required|integer',
+            'stok' => 'required|integer', 
+            'isReadyPublish' => 'required'
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(
+                $validator->errors(), HttpFoundationResponse::HTTP_UNPROCESSABLE_ENTITY
+            );
+        }
+
+        try {
+            $products = Product::create($request->except($except_data));
+            $response = [
+                'message' => 'Create Shoes Data',
+                'data' => $products,
+            ];
+
+            return response()->json($response, HttpFoundationResponse::HTTP_CREATED);
+        } catch (QueryException $e) {
+            return response()->json([
+                'message' => "Failed! {$e->errorInfo}",
+            ]);
+        }
     }
 
     /**
@@ -42,7 +72,8 @@ class ProductControllerAPI extends Controller
      */
     public function show($id)
     {
-        //
+        $products = Product::select('*')->find($id);
+        return response()->json($products, 200);
     }
 
     /**
@@ -54,7 +85,39 @@ class ProductControllerAPI extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        // $except_data = ['id', 'created_at', 'updated_at'];
+        $product = Product::find($id);
+
+        $validator = Validator::make($request->all(), [
+            'nama' => 'required|string', 
+            'harga' => 'required|integer',
+            'stok' => 'required|integer', 
+            'isReadyPublish' => 'required'
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(
+                $validator->errors(), HttpFoundationResponse::HTTP_UNPROCESSABLE_ENTITY
+            );
+        }
+
+        $update_product = $product->update($request->all());
+
+        if ($update_product) {
+            return response()->json([
+                'success' => true,
+                'message' => 'Update Shoes Data',
+                'data' => $product
+            ]);
+        }
+
+        else {
+            return response()->json([
+                'success' => false,
+                'message' => "Failed! {$e->errorInfo}",
+                'data' => null
+            ]);
+        }
     }
 
     /**
@@ -65,6 +128,11 @@ class ProductControllerAPI extends Controller
      */
     public function destroy($id)
     {
-        //
+        $product = Product::find($id)->delete();
+        return response()->json([
+            'success' => true,
+            'message' => 'Shoes Data successfully deleted!',
+            'data' => $product
+        ]);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -19,4 +19,8 @@ use App\Http\Controllers\ProductControllerAPI;
 //     return $request->user();
 // });
 
-Route::get('/shoes', [ProductControllerAPI::class, 'index']);
+Route::get('/shoes', [ProductControllerAPI::class, 'index'])->name('shoes.index');
+Route::post('/shoes', [ProductControllerAPI::class, 'store'])->name('shoes.store');
+Route::get('/shoes/{id}', [ProductControllerAPI::class, 'show'])->name('shoes.show');
+Route::put('/shoes/edit/{id}', [ProductControllerAPI::class, 'update'])->name('shoes.update');
+Route::delete('/shoes/{id}', [ProductControllerAPI::class, 'destroy'])->name('shoes.delete');
